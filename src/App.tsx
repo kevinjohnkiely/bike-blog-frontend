e5873e0c3d25d4d018d6e52a779e3e719dae2dd3
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2023-11-05 15:28:15 +0000

    Fetching posts and proxy url setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Post } from './models/post';
 
 function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    async function loadPosts() {
+      try {
+        const response = await fetch('/api/posts', {
+          method: 'GET',
+        });
+        const posts = await response.json();
+        setPosts(posts);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+    loadPosts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {JSON.stringify(posts)}
     </div>
   );
 }
